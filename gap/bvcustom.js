6963b08c94b9b1c435e90cd5e2d77cb6c80c6985
Brian Adams <brian@brianadamsdesigns.com>
2014-04-18 15:06:55 -0500

    test

--- gap/bvcustom.js
+++ gap/bvcustom.js
@@ -1,10 +1,22 @@
 <script>
-	bvcustom = {
+	var bvcustom = {
 		loaded : false,
 		loadCustomCSS : function (client) {
+			var url = "https://rawgit.com/brianadamsdesigns/bvcustomcss/master/" + client + "/bvcustom.css"
+			var link = document.createElement('link');
+			link.type = "text/css";
+			link.rel = "stylesheet";
+			link.href = url;
 			var head = document.getElementsByTagName('head')[0];
-			head[0].write("<link type='text/css' rel='stylesheet' href='https://rawgit.com/brianadamsdesigns/bvcustomcss/master/" + client + "/bvcustom.css'>");
+			head.appendChild(link);
+
+			link.onload = link.onreadystatechange = function() {
+				if (!bvcustom.loaded && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+					bvcustom.loaded = true;
+					link.onload = link.onreadystatechange = null;
+				};
+			};
 		}
-	}
-	(bvcustom.loaded) ? bvcustom.loadCustomCSS('gap') : return;
+	};
+	(!bvcustom.loaded) ? bvcustom.loadCustomCSS('gap') : bvcustom.loaded = false;
 </script>
